e6d5a54fb632b218f8d1d439535d2c641d203fef
newmanw159 <57231370+newmanw159@users.noreply.github.com>
2020-04-11 11:30:03 -0500

    IDK why this doesn't work as it should

---
+++ project_helper_func.py
@@ -0,0 +1,112 @@
+import scipy.linalg as linalg
+import numpy as np
+import math
+import modern_robotics as mr
+'''
+Input:  list of 6 joint angles
+Output: Forward Kinemetics matrix
+'''
+def forward_ur3_kin(angles):
+    T = []
+    M =np.matrix([[1,0,0,-0.2700],\
+                  [0,1,0,0.01],\
+                  [0,0,1,0.652],\
+                  [0,0,0,1]])
+    W = [[0, -1, -1, -1,  0, -1],\
+        [ 0,  0,  0,  0,  0,  0],\
+        [ 1,  0,  0,  0,  1,  0]]
+    '''
+    using coppela measurements
+    Q = [[0.00012,-0.1115,-0.1115,-0.1115,-0.1122,-0.1115],\
+        [0.000086,0.000054,0.00013,0.000085,0.000085,0.000085],\
+        [0.1045,0.1089,0.3525,0.5658,0.6500,0.6511]]
+    '''
+    #using lab measurements
+    Q =[[0.0,-0.120,-0.120,-0.027,-0.110,-0.110],\
+        [0.0, 0.0,   0.0,   0.0,   0.0,   0.0],\
+        [0.0, 0.152, 0.396, 0.609, 0.609, 0.692]]
+
+    V = [[0,0,0,0,0,0],\
+        [0,0,0,0,0,0],\
+        [0,0,0,0,0,0]]
+    #v=cross(-w,q)
+    for idx in range(0,6):
+        V[0][idx]= -W[1][idx]*Q[2][idx]+W[2][idx]*Q[1][idx]
+        V[1][idx]= -W[2][idx]*Q[0][idx]+W[0][idx]*Q[2][idx]
+        V[2][idx]= -W[0][idx]*Q[1][idx]+W[1][idx]*Q[0][idx]
+
+    S =[[W[0][0],W[0][1],W[0][2],W[0][3],W[0][4],W[0][5]],\
+        [W[1][0],W[1][1],W[1][2],W[1][3],W[1][4],W[1][5]],\
+        [W[2][0],W[2][1],W[2][2],W[2][3],W[2][4],W[2][5]],\
+        [V[0][0],V[0][1],V[0][2],V[0][3],V[0][4],V[0][5]],\
+        [V[1][0],V[1][1],V[1][2],V[1][3],V[1][4],V[1][5]],\
+        [V[2][0],V[2][1],V[2][2],V[2][3],V[2][4],V[2][5]]]
+    
+
+    for joint in range(0,6):
+        skew_S = [[0,            -1*S[2][joint],  S[1][joint],   S[3][joint]],\
+                 [ S[2][joint],   0,             -1*S[0][joint], S[4][joint]],\
+                 [-1*S[1][joint], S[0][joint],    0,             S[5][joint]],\
+                 [ 0,             0,              0,             0]]
+        T.append(linalg.expm((np.matrix(skew_S))*(angles[joint])))
+    '''    
+    for i in range(len(T)):
+        print('T_',i,'\n',T[i],'\n')
+    print(T[0]@T[1]@T[2]@T[3]@T[4]@T[5])
+    '''
+    T_final = T[0]@T[1]@T[2]@T[3]@T[4]@T[5]@M
+    return(T_final)
+
+
+
+def inverseKin(final_endeffector,thetas):
+    
+    thetas0 = [0, 0, 0, 0, 0, 0]
+
+    tolerances = 0.0001
+
+    M =np.array([[1,0,0,-0.2700],\
+                 [0,1,0,0.01],\
+                 [0,0,1,0.652],\
+                 [0,0,0,1]])
+    W = [[0, -1, -1, -1,  0, -1],\
+        [ 0,  0,  0,  0,  0,  0],\
+        [ 1,  0,  0,  0,  1,  0]]
+
+    #using lab measurements
+    Q =[[0.0,-0.120,-0.120,-0.027,-0.110,-0.110],\
+        [0.0, 0.0,   0.0,   0.0,   0.0,   0.0],\
+        [0.0, 0.152, 0.396, 0.609, 0.609, 0.692]]
+
+    V = [[0,0,0,0,0,0],\
+        [0,0,0,0,0,0],\
+        [0,0,0,0,0,0]]
+    #v=cross(-w,q)
+    for idx in range(0,6):
+        V[0][idx]= -W[1][idx]*Q[2][idx]+W[2][idx]*Q[1][idx]
+        V[1][idx]= -W[2][idx]*Q[0][idx]+W[0][idx]*Q[2][idx]
+        V[2][idx]= -W[0][idx]*Q[1][idx]+W[1][idx]*Q[0][idx]
+
+    S =np.array([[W[0][0],W[0][1],W[0][2],W[0][3],W[0][4],W[0][5]],\
+                 [W[1][0],W[1][1],W[1][2],W[1][3],W[1][4],W[1][5]],\
+                 [W[2][0],W[2][1],W[2][2],W[2][3],W[2][4],W[2][5]],\
+                 [V[0][0],V[0][1],V[0][2],V[0][3],V[0][4],V[0][5]],\
+                 [V[1][0],V[1][1],V[1][2],V[1][3],V[1][4],V[1][5]],\
+                 [V[2][0],V[2][1],V[2][2],V[2][3],V[2][4],V[2][5]]])
+
+    theta,success = mr.IKinSpace(S,M,final_endeffector,thetas,tolerances,tolerances)
+    if(success == False):
+        print('Warning not in tolerances \n ')
+        res = input('Continue? y/n:')
+        if(res == 'n'):
+            print('press ctrl + c')
+            while(1):
+                a=1
+    theta_return = []
+    for ang in theta:
+        ang_sign = ang/abs(ang)
+        while(abs(ang) <= (2*np.pi)):
+            ang=abs(ang)-(2*np.pi)
+            print(ang)
+        theta_return.append(ang_sign*ang)
+    return(theta_return)
---
+++ project_update_1.py
@@ -0,0 +1,96 @@
+import numpy
+import sim
+import time
+import sys
+import math
+import project_helper_func
+
+
+print('program start')
+sim.simxFinish(-1) # just in case, close all opened connections
+clientID=sim.simxStart('127.0.0.1',19999,True,True,5000,5) # Connect to CoppeliaSim
+if clientID==-1:
+    print ('Not connected to remote API server')
+    sys.exit()
+else:
+    print('Connected to remote API server')
+
+FvI = input('F for forward Kinematics, I for Inverse Kinematics')
+if(FvI=='F' or FvI == 'f'):
+    print("input 6 space seperated angles in degrees")
+    theta = [float(i)*numpy.pi/180 for i in input().split()]
+    #print(theta)
+    location = project_helper_func.forward_ur3_kin(theta)
+    print('location \n',location)
+    XYZ = [location.item(3),location.item(7),location.item(11)]
+    #sim.simxAddStatusbarMessage(clientID,'XYZ Pos',sim.simx_opmode_oneshot)
+    #sim.simxAddStatusbarMessage(clientID,location,sim.simx_opmode_oneshot)
+if(FvI=='I' or FvI == 'i'):
+    theta_corrected = []
+    print("input 3 space seperated X Y Z values in Meters")
+    target = [float(i) for i in input().split()]
+    final_endeffector= numpy.array([[ 0.77309907, -0.5458005,   0.32314029, -0.20719392],\
+                                    [ 0.62340519,  0.55988193, -0.5458005,  -0.21503198],\
+                                    [ 0.11697778,  0.62340519,  0.77309907,  0.62887313],\
+                                    [ 0.0        ,  0.0        ,  0.0        ,  1.0]])
+    thetas = [20, -20, 20, -20, 20, -20]
+    theta = project_helper_func.inverseKin(final_endeffector,thetas)
+    #print(theta)
+    for angle in theta:
+        theta_corrected.append( (angle%(2*numpy.pi))*(2*numpy.pi))
+    print(theta_corrected)
+
+velocities = [1,1,1,1,1,1]
+error_code, POI = sim.simxGetObjectHandle(clientID,'POI',sim.simx_opmode_blocking)
+#print ('errorcodePOI =',error_code)
+error_code, UR3_joint1 = sim.simxGetObjectHandle(clientID,'UR3_joint1',sim.simx_opmode_blocking)
+#print ('errorcode1 =',error_code)
+error_code, UR3_joint2 = sim.simxGetObjectHandle(clientID,'UR3_joint2',sim.simx_opmode_blocking)
+#print ('errorcode2 =',error_code)
+error_code, UR3_joint3 = sim.simxGetObjectHandle(clientID,'UR3_joint3',sim.simx_opmode_blocking)
+#print ('errorcode3 =',error_code)
+error_code, UR3_joint4 = sim.simxGetObjectHandle(clientID,'UR3_joint4',sim.simx_opmode_blocking)
+#print ('errorcode4 =',error_code)
+error_code, UR3_joint5 = sim.simxGetObjectHandle(clientID,'UR3_joint5',sim.simx_opmode_blocking)
+#print ('errorcode5 =',error_code)
+error_code, UR3_joint6 = sim.simxGetObjectHandle(clientID,'UR3_joint6',sim.simx_opmode_blocking)
+#print ('errorcode6 =',error_code)
+#error_code, Gripper = sim.simxGetObjectHandle(clientID,'Gripper',sim.simx_opmode_blocking)
+
+#sim.simxStartSimulation(clientID,sim.simx_opmode_blocking)
+return_code = sim.simxSetJointTargetVelocity(clientID,UR3_joint1,velocities[0],sim.simx_opmode_blocking)
+return_code = sim.simxSetJointTargetVelocity(clientID,UR3_joint2,velocities[1],sim.simx_opmode_blocking)
+return_code = sim.simxSetJointTargetVelocity(clientID,UR3_joint3,velocities[2],sim.simx_opmode_blocking)
+return_code = sim.simxSetJointTargetVelocity(clientID,UR3_joint4,velocities[3],sim.simx_opmode_blocking)
+return_code = sim.simxSetJointTargetVelocity(clientID,UR3_joint5,velocities[4],sim.simx_opmode_blocking)
+return_code = sim.simxSetJointTargetVelocity(clientID,UR3_joint6,velocities[5],sim.simx_opmode_blocking)
+
+#sim.simxPauseCommunication(clientID,True)
+return_code = sim.simxSetJointTargetPosition(clientID,UR3_joint1,theta[0],sim.simx_opmode_blocking)
+#print('returncode1=',return_code)
+return_code = sim.simxSetJointTargetPosition(clientID,UR3_joint2,theta[1],sim.simx_opmode_blocking)
+#print('returncode2=',return_code)
+return_code = sim.simxSetJointTargetPosition(clientID,UR3_joint3,theta[2],sim.simx_opmode_blocking)
+#print('returncode3=',return_code)
+return_code = sim.simxSetJointTargetPosition(clientID,UR3_joint4,theta[3],sim.simx_opmode_blocking)
+#print('returncode4=',return_code)
+return_code = sim.simxSetJointTargetPosition(clientID,UR3_joint5,theta[4],sim.simx_opmode_blocking)
+#print('returncode5=',return_code)
+return_code = sim.simxSetJointTargetPosition(clientID,UR3_joint6,theta[5],sim.simx_opmode_blocking)
+#print('returncode6=',return_code)
+#sim.simxPauseCommunication(clientID,False)
+if(FvI=='F' or FvI == 'f'):
+    sim.simxSetObjectPosition(clientID,POI,-1,XYZ,sim.simx_opmode_blocking)
+
+sim.simxAddStatusbarMessage(clientID,'End of Code',sim.simx_opmode_oneshot)
+print('End of Code')
+res = input('reset? y/n:')
+#print(res)
+if(res == 'y'or res ==1):
+    return_code = sim.simxSetJointTargetPosition(clientID,UR3_joint1,0,sim.simx_opmode_blocking)
+    return_code = sim.simxSetJointTargetPosition(clientID,UR3_joint2,0,sim.simx_opmode_blocking)
+    return_code = sim.simxSetJointTargetPosition(clientID,UR3_joint3,0,sim.simx_opmode_blocking)
+    return_code = sim.simxSetJointTargetPosition(clientID,UR3_joint4,0,sim.simx_opmode_blocking)
+    return_code = sim.simxSetJointTargetPosition(clientID,UR3_joint5,0,sim.simx_opmode_blocking)
+    return_code = sim.simxSetJointTargetPosition(clientID,UR3_joint6,0,sim.simx_opmode_blocking)
+    return_code = sim.simxSetObjectPosition(clientID,POI,-1,[-0.2700,0.01,0.652],sim.simx_opmode_blocking)
